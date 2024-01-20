c94f9bfca6fce7e9d3799ffc7777c2d000e29370
Rizal Purnomo <48096288+rizalp3@users.noreply.github.com>
2024-01-20 22:21:10 +0700

    Add swiper js library

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import { createApp } from 'vue';
+
+// Vue Feather
 import VueFeather from 'vue-feather';
 
+// Boostrap
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// VCalendar
 import VCalendar from 'v-calendar';
 import 'v-calendar/style.css';
 
-import './assets/main.css';
+// Swiper
+import 'swiper/css/bundle';
 
+// Vue Router
 import router from './router';
+
+// Base Style
+import './assets/main.css';
 
 import App from './App.vue';
 
