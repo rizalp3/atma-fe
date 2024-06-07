b532597f49db5dd90af7ba7273251389ff4d2c1d
rizalp3 <rizalp3@gmail.com>
2024-06-07 09:00:53 +0700

    Add markdown-it as a component

---
+++ src/components/atma/AtmaMarkdown.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-html="markdown.render(content)"></div>
+</template>
+
+<script>
+import MarkdownIt from 'markdown-it';
+
+export default {
+    name: 'AtmaMarkdown',
+
+    props: {
+        content: {
+            type: String,
+            default: ''
+        }
+    },
+
+    setup() {
+        const markdown = new MarkdownIt();
+        return { markdown };
+    }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -30,6 +30,7 @@ import App from './App.vue';
 import AtmaButton from './components/atma/AtmaButton.vue';
 import AtmaButtonIcon from './components/atma/AtmaButtonIcon.vue';
 import AtmaLinkPreview from './components/atma/AtmaLinkPreview.vue';
+import AtmaMarkdown from './components/atma/AtmaMarkdown.vue';
 import AtmaModal from './components/atma/AtmaModal.vue';
 import AtmaSkeleton from './components/atma/AtmaSkeleton.vue';
 import AtmaText from './components/atma/AtmaText.vue';
@@ -52,6 +53,7 @@ app.component(VueFeather.name, VueFeather);
 app.component('AtmaButton', AtmaButton)
     .component('AtmaButtonIcon', AtmaButtonIcon)
     .component('AtmaLinkPreview', AtmaLinkPreview)
+    .component('AtmaMarkdown', AtmaMarkdown)
     .component('AtmaModal', AtmaModal)
     .component('AtmaSkeleton', AtmaSkeleton)
     .component('AtmaText', AtmaText);
