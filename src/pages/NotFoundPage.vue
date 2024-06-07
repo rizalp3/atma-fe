bd9bd454fb677ddd48f58e0eed25f98619f13923
rizalp3 <rizalp3@gmail.com>
2024-06-07 08:11:35 +0700

    Update not found to be a separated component

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,27 +1,15 @@
 <template>
-    <div class="not-found">
-        <img
-            class="not-found__image"
-            alt="Not Found Illustration"
-            :src="Illustration"
-        />
-
-        <h1 class="not-found__title">Page Not Found</h1>
-
-        <p class="not-found__subtitle">
-            We Can't Find The Page You're Looking For
-        </p>
-
-        <router-link class="not-found__action" to="/">Back to Home</router-link>
+    <div class="not-found-page">
+        <not-found />
     </div>
 </template>
 
 <script setup>
-import Illustration from '@/assets/image/not-found.svg';
+import NotFound from '@/components/NotFound.vue';
 </script>
 
 <style lang="scss" scoped>
-.not-found {
+.not-found-page {
     position: fixed;
     inset: 0;
 
@@ -30,32 +18,5 @@ import Illustration from '@/assets/image/not-found.svg';
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
-
-    text-align: center;
-
-    &__image {
-        width: 80%;
-        max-width: 240px;
-    }
-
-    &__title {
-        @include text(30px);
-
-        margin: 36px 0 0;
-        font-weight: 700;
-        color: #252525;
-    }
-
-    &__subtitle {
-        margin: 8px 0 36px;
-        font-weight: 300;
-        color: #8a8a8a;
-    }
-
-    &__action {
-        text-decoration: none;
-        color: var(--victoria-600);
-    }
 }
 </style>
