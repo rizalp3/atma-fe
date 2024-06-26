50c928735187db958d38e63a31fef226cc61517d
rizalp3 <rizalp3@gmail.com>
2024-06-26 15:44:12 +0700

    Add register modal

---
+++ src/components/auth/Register.vue
@@ -0,0 +1,370 @@
+<template>
+    <div class="register-section">
+        <div class="register-section__title">Register</div>
+
+        <div class="register-section__subtitle">
+            Please provide your details to sign up
+        </div>
+
+        <form @submit.prevent="handleSubmit">
+            <!-- Name -->
+            <div class="register-section__input">
+                <label class="form-label" for="register-name">Name</label>
+                <input
+                    v-model="name"
+                    :class="['form-control', { 'is-invalid': errors.name }]"
+                    type="text"
+                    placeholder="Enter your name"
+                    id="register-name"
+                    @change="validateName"
+                />
+                <div class="invalid-feedback">{{ errors.name }}</div>
+            </div>
+
+            <!-- Email -->
+            <div class="register-section__input">
+                <label class="form-label" for="register-email">
+                    Email Address
+                </label>
+                <input
+                    v-model="email"
+                    :class="['form-control', { 'is-invalid': errors.email }]"
+                    type="email"
+                    placeholder="Enter your email address"
+                    id="register-email"
+                    @change="validateEmail"
+                />
+                <div class="invalid-feedback">{{ errors.email }}</div>
+            </div>
+
+            <!-- Password -->
+            <div class="register-section__input">
+                <label class="form-label" for="register-password">
+                    Password
+                </label>
+
+                <input
+                    v-model="password"
+                    :class="[
+                        'form-control',
+                        'form-control--password',
+                        { 'is-invalid': errors.password }
+                    ]"
+                    :type="toggleType"
+                    placeholder="Password"
+                    id="register-password"
+                    @change="validatePassword"
+                />
+                <button
+                    class="register-section__password-toggle"
+                    type="button"
+                    tabindex="-1"
+                    @click="handlePasswordVisibility"
+                >
+                    <atma-icon :name="toggleIcon" size="20" />
+                </button>
+
+                <div class="register-section__rule-list">
+                    <div
+                        v-for="(rule, i) in rules"
+                        :key="`password-rule-${i}`"
+                        :class="[
+                            'register-section__rule',
+                            { 'register-section__rule--passed': rule.passed }
+                        ]"
+                    >
+                        <atma-icon
+                            :name="rule.passed ? 'check' : 'close'"
+                            size="20"
+                        />
+
+                        <div class="register-section__rule-text">
+                            {{ rule.message }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Submit -->
+            <button class="register-section__submit" type="submit">
+                Register
+            </button>
+
+            <!-- Login -->
+            <div class="register-section__login">
+                <div>Already have an account?</div>
+
+                <button type="button">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RegisterSection',
+
+    data() {
+        return {
+            isPasswordShown: false,
+
+            name: '',
+            email: '',
+            password: '',
+
+            errors: {},
+            rules: [
+                { passed: false, message: 'At least 8 characters' },
+                { passed: false, message: 'Contain 1 uppercase character' },
+                { passed: false, message: 'Contain 1 number' },
+                { passed: false, message: `Can't contain any spaces` }
+            ]
+        };
+    },
+
+    watch: {
+        name() {
+            if (this.errors.name) {
+                delete this.errors.name;
+            }
+        },
+        email() {
+            if (this.errors.email) {
+                delete this.errors.email;
+            }
+        },
+        password() {
+            if (this.errors.password) {
+                delete this.errors.password;
+            }
+
+            this.checkPassword();
+        }
+    },
+
+    computed: {
+        toggleIcon() {
+            return this.isPasswordShown ? 'visibility-off' : 'visibility';
+        },
+        toggleType() {
+            return this.isPasswordShown ? 'text' : 'password';
+        }
+    },
+
+    methods: {
+        validateName() {
+            if (this.name === '') {
+                this.errors.name = `Name is required`;
+            }
+        },
+
+        validateEmail() {
+            const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi;
+
+            if (this.email === '') {
+                this.errors.email = 'Email is required';
+            } else if (!emailRegex.test(this.email)) {
+                this.errors.email = 'Invalid email address';
+            }
+        },
+
+        validatePassword() {
+            const isPasswordValid = this.rules.every((rule) => rule.passed);
+
+            if (!isPasswordValid) {
+                this.errors.password = 'Password is not valid';
+            }
+        },
+
+        checkPassword() {
+            if (this.password === '') {
+                this.rules.forEach((rule) => {
+                    rule.passed = false;
+                });
+            } else {
+                this.rules[0].passed = this.password.length >= 8;
+                this.rules[1].passed = /[A-Z]/.test(this.password);
+                this.rules[2].passed = /\d/.test(this.password);
+                this.rules[3].passed = !/\s/.test(this.password);
+            }
+        },
+
+        handlePasswordVisibility() {
+            this.isPasswordShown = !this.isPasswordShown;
+        },
+
+        handleSubmit() {
+            this.validateName();
+            this.validateEmail();
+            this.validatePassword();
+
+            if (Object.keys(this.errors).length <= 0) {
+                console.log({
+                    name: this.name,
+                    email: this.email,
+                    password: this.password
+                });
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.register-section {
+    &__title {
+        @include text(24px, 700);
+        text-align: center;
+        color: var(--system-color-on-surface);
+    }
+
+    &__subtitle {
+        @include text(14px, 400);
+        text-align: center;
+        color: var(--system-color-outline);
+        margin: 4px 0 32px;
+    }
+
+    &__input {
+        position: relative;
+        margin-bottom: 16px;
+
+        .form-label {
+            @include text(14px, 500);
+            margin-bottom: 6px;
+            color: var(--system-color-on-surface);
+        }
+
+        .form-control {
+            font-size: 14px;
+            line-height: 24px;
+
+            background: var(--system-color-surface);
+            color: var(--system-color-on-surface);
+            border-color: var(--system-color-surface-container-highest);
+
+            &:focus {
+                box-shadow: none;
+                border-color: var(--system-color-outline);
+            }
+
+            &:hover:not(:focus, .is-invalid) {
+                border-color: var(--system-color-outline-variant);
+            }
+
+            &::placeholder {
+                color: var(--system-color-outline);
+            }
+
+            &.is-invalid {
+                border-color: var(--system-color-error);
+            }
+
+            &--password:focus {
+                ~ .invalid-feedback {
+                    display: none;
+                }
+
+                ~ .register-section__rule-list {
+                    .register-section__rule {
+                        color: var(--system-color-error);
+
+                        &--passed {
+                            color: var(--system-color-valid);
+                        }
+                    }
+                }
+            }
+        }
+
+        .invalid-feedback {
+            --bs-form-invalid-color: var(--system-color-error);
+        }
+    }
+
+    &__password-toggle {
+        width: 38px;
+        height: 38px;
+
+        position: absolute;
+        top: 29px;
+        right: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 6px;
+
+        color: var(--system-color-outline);
+
+        &:hover {
+            color: var(--system-color-on-surface);
+        }
+    }
+
+    &__rule {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+
+        color: var(--system-color-outline);
+
+        &-list {
+            display: flex;
+            flex-direction: column;
+            gap: 4px;
+            margin-top: 8px;
+        }
+
+        &-text {
+            @include text(14px, 400);
+        }
+
+        &--passed {
+            color: var(--system-color-valid);
+        }
+    }
+
+    &__submit {
+        width: 100%;
+
+        padding: 8px 16px;
+        margin-top: 32px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 20px;
+
+        @include text(14px, 500);
+
+        background: var(--system-color-primary);
+        color: var(--system-color-on-primary);
+    }
+
+    &__login {
+        @include text(14px, 400);
+
+        margin-top: 16px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 4px;
+
+        color: var(--system-color-outline);
+
+        button {
+            font-weight: 500;
+            color: var(--system-color-on-surface);
+            cursor: pointer;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+}
+</style>
