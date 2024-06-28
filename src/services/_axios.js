b467e40786076283282d6ab20424b03953504c69
rizalp3 <rizalp3@gmail.com>
2024-06-28 16:15:12 +0700

    Update authorization for axios

--- src/services/_axios.js
+++ src/services/_axios.js
@@ -2,24 +2,44 @@ import axios from 'axios';
 import qs from 'qs';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-axios.defaults.headers = {
-    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
-};
 
-const request = async (method, url, config) => {
-    const strapiQuery = qs.stringify(config.strapi);
-    const data = config.data;
+const request = async (method, url, data) => {
+    // -- Initialization --
+    const config = {};
+
+    // -- Strapi Query --
+    const strapiQuery = qs.stringify(data.strapi);
 
     if (strapiQuery) {
         url += `?${strapiQuery}`;
     }
 
+    // -- Authorization --
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        config.headers = { Authorization: `Bearer ${token}` };
+    } else {
+        config.headers = {
+            Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
+        };
+    }
+
+    // -- Post / Get Method --
     let response;
 
-    try {
-        response = await axios[method](url, data);
-    } catch (error) {
-        response = error.response;
+    if (data.data) {
+        try {
+            response = await axios[method](url, data.data, config);
+        } catch (error) {
+            response = error.response;
+        }
+    } else {
+        try {
+            response = await axios[method](url, config);
+        } catch (error) {
+            response = error.response;
+        }
     }
 
     return response?.data || {};
