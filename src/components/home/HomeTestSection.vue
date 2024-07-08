97a4db25be1ca9b9fe1942067890e19c3b5a3eb9
rizalp3 <rizalp3@gmail.com>
2024-07-08 15:19:46 +0700

    Add API integration for community and test section in homepage

--- src/components/home/HomeTestSection.vue
+++ src/components/home/HomeTestSection.vue
@@ -4,88 +4,72 @@
             Test Result
         </atma-text>
 
-        <div class="home-test__item">
-            <div class="home-test__icon">
-                <vue-feather type="user" size="24" />
-            </div>
-
-            <div>
-                <atma-text size="14" weight="500" color-scheme="on-surface">
-                    Self Test
-                </atma-text>
-                <atma-text size="14" weight="400" color-scheme="outline">
-                    Yesterday
-                </atma-text>
-            </div>
-
-            <div class="home-test__result">Level 4</div>
-        </div>
-
-        <div class="home-test__item">
-            <div class="home-test__icon home-test__icon--others">
-                <vue-feather type="users" size="24" />
-            </div>
-
-            <div>
-                <atma-text size="14" weight="500" color-scheme="on-surface">
-                    Rantty
-                </atma-text>
-                <atma-text size="14" weight="400" color-scheme="outline">
-                    2 days ago
-                </atma-text>
-            </div>
-
-            <div class="home-test__result">Level 2</div>
+        <div class="home-test__wrapper">
+            <test-result
+                v-for="(result, i) in results"
+                :key="`result-${i}`"
+                :result="result"
+            />
         </div>
     </div>
 </template>
 
 <script>
+import endpoint from '@/services/report';
+
+import TestResult from '@/components/report/TestResult.vue';
+
 export default {
-    name: 'HomeTestSection'
+    name: 'HomeTestSection',
+
+    components: {
+        TestResult
+    },
+
+    data() {
+        return {
+            results: []
+        };
+    },
+
+    mounted() {
+        this.getTestResults();
+    },
+
+    methods: {
+        async getTestResults() {
+            const config = { pagination: { limit: 3 } };
+
+            const response = await endpoint.getTestResults(config);
+
+            if (response.data) {
+                this.results = response.data.map((result) => ({
+                    id: result.id,
+                    type: result.attributes.type,
+                    name: result.attributes.name,
+                    value: result.attributes.value,
+                    time: result.attributes.createdAt
+                }));
+            }
+        }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
 .home-test {
-    display: flex;
-    flex-direction: column;
-
-    gap: 16px;
     padding: 18px 20px 20px;
     border-radius: 16px;
-
     background: var(--system-color-surface);
 
-    &__item {
-        display: flex;
-        align-items: center;
-        gap: 12px;
+    > *:first-child {
+        margin-bottom: 16px;
     }
 
-    &__icon {
-        width: 48px;
-        height: 48px;
-        border-radius: 50%;
-
+    &__wrapper {
         display: flex;
         flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        background: rgba(242, 153, 74, 0.15);
-        color: #f2994a;
-
-        &--others {
-            background: rgba(45, 156, 219, 0.15);
-            color: #2d9cdb;
-        }
-    }
-
-    &__result {
-        @include text(12px, 500);
-        margin-left: auto;
-        color: var(--system-color-outline);
+        gap: 20px;
     }
 }
 </style>
